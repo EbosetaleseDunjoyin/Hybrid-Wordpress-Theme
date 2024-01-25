d93dd64bb724a9be885dd58c7b8d91e0d7b76dc0
EbosetaleseDunjoyin <eboselese@gmail.com>
2024-01-25 23:02:13 +0100

    bs5navwalker

--- js/customizer.js
+++ js/customizer.js
@@ -7,36 +7,67 @@
  * Contains handlers to make Theme Customizer preview reload changes asynchronously.
  */
 
-( function( $ ) {
-	// Site title and description.
-	wp.customize( 'blogname', function( value ) {
-		value.bind( function( to ) {
-			$( '.site-title a' ).text( to );
-		} );
-	} );
-	wp.customize( 'blogdescription', function( value ) {
-		value.bind( function( to ) {
-			$( '.site-description' ).text( to );
-		} );
-	} );
+(function ($) {
+  // Site title and description.
+  wp.customize("blogname", function (value) {
+    value.bind(function (to) {
+      $(".site-title a").text(to);
+    });
+  });
+  wp.customize("blogdescription", function (value) {
+    value.bind(function (to) {
+      $(".site-description").text(to);
+    });
+  });
 
-	// Header text color.
-	wp.customize( 'header_textcolor', function( value ) {
-		value.bind( function( to ) {
-			if ( 'blank' === to ) {
-				$( '.site-title, .site-description' ).css( {
-					clip: 'rect(1px, 1px, 1px, 1px)',
-					position: 'absolute',
-				} );
-			} else {
-				$( '.site-title, .site-description' ).css( {
-					clip: 'auto',
-					position: 'relative',
-				} );
-				$( '.site-title a, .site-description' ).css( {
-					color: to,
-				} );
-			}
-		} );
-	} );
-}( jQuery ) );
+  wp.customize("fp_header_toptext", function (value) {
+    value.bind(function (to) {
+      $(".header-smtext").text(to);
+    });
+  });
+  wp.customize("fp_header_btmtext", function (value) {
+    value.bind(function (to) {
+      $(".header-text").text(to);
+    });
+  });
+  wp.customize("fp_header_button", function (value) {
+    value.bind(function (to) {
+      $(".header-button").text(to);
+    });
+  });
+  wp.customize("bp_header_toptext", function (value) {
+    value.bind(function (to) {
+      $(".header-smtext").text(to);
+    });
+  });
+  wp.customize("bp_header_btmtext", function (value) {
+    value.bind(function (to) {
+      $(".header-text").text(to);
+    });
+  });
+  wp.customize("bp_header_button", function (value) {
+    value.bind(function (to) {
+      $(".header-button").text(to);
+    });
+  });
+
+  // Header text color.
+  wp.customize("header_textcolor", function (value) {
+    value.bind(function (to) {
+      if ("blank" === to) {
+        $(".site-title, .site-description").css({
+          clip: "rect(1px, 1px, 1px, 1px)",
+          position: "absolute",
+        });
+      } else {
+        $(".site-title, .site-description").css({
+          clip: "auto",
+          position: "relative",
+        });
+        $(".site-title a, .site-description").css({
+          color: to,
+        });
+      }
+    });
+  });
+})(jQuery);
